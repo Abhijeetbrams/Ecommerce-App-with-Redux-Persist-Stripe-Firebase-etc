400297ee37d4975c10e82fc6428d26c4c9bc97bf
Sharad Chandra <abybrams@gmail.com>
2020-09-05 22:26:13 +0530

    Adding Logger mIddleware on for development environment and Application Hosting through Heroku

--- src/Components/Redux/store.js
+++ src/Components/Redux/store.js
@@ -9,6 +9,16 @@ const middlewares = [logger]; // Middleware to our store so that whenever  actio
 //Middleware is that recieve action in and then do something with them and then pass them out to the root reducer.
 // Middleware is an array that contains middlewares
 
+if (process.env.NODE_ENV === 'development') {// Inside of Node.js there is a environment and Create React app sets an
+    //environment variable which can only be accessed through this process and NODE_ENV will be development,production or test
+    // it helps to know whether or not our app is being served in development or on which env.
+    // and when we call "npm build" means NODE_ENV is "Production".
+    // and when we type "npm start" and host it on our local server then this node environment variable to "Development"
+    middlewares.push(logger);
+  }
+
+
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));// passing all the element of the middlewares array
 // as indiviual argument and we're instantiating store object via createStore method.
 
